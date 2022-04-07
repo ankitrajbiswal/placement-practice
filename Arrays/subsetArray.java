6de640b755a3832b9bbefb10e7079db5b15bc4b5
Ankit Raj Biswal <ankitrudra2001@gmail.com>
2022-04-07 08:17:25 -0700

    Update subsetArray.java

--- Arrays/subsetArray.java
+++ Arrays/subsetArray.java
@@ -1,28 +1,31 @@
 import java.util.*;
 public class subsetArray {
     public static void main (String [] args){
-        int arr1[]={11, 1, 13, 21, 3, 7};
-        int arr2[]={11, 3, 7, 1};
-        int n=arr1.length;
-        int m=arr2.length;
+        long arr1[]={11, 1, 13, 21, 3, 7};
+        long arr2[]={11, 3, 7, 1};
+        long n=arr1.length;
+        long m=arr2.length;
 
         Arraysubset(arr1,arr2,n,m);
     }
 
-    static boolean Arraysubset(int arr1[],int arr2[], int n,int m){
-        HashSet <Integer> hset=new HashSet<>();
+    static String Arraysubset(long arr1[],long arr2[], long n,Long m){
+        HashSet <Long> hs=new HashSet<>();
+        long temp =0;
+        
+        
+        for(int i =0 ; i<n ; i++)
+        hs.add(arr1[i]);
 
-        // Storing all values of arr1 on hset
-        for(int i=0;i<m;i++){
-            if(!hset.contains(arr1[i]))
-            hset.add(arr1[i]);
-        }
+        for(int i =0 ; i<m;i++)
+        if(hs.contains(arr2[i]))
+        temp++;
+            
+        if(temp == arr2.length)
+        return "Yes";
+    
+        return "No";
 
-        for(int i=0;i<n;i++){
-            if(!hset.contains(arr2[i]));
-            return false;
-        }
-        return true;
        
     }
     
