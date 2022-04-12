3c7fd30a22c0c47567bae4fa9f4920b20ffa3ee0
Ankit Raj Biswal <ankitrudra2001@gmail.com>
2022-04-12 10:22:16 +0530

    Add files via upload

---
+++ Placement Codes/Bit Manipulation/squareofnumberwithoutusingpow.java
@@ -0,0 +1,22 @@
+public class squareofnumberwithoutusingpow {
+    public static void main(String [] args) {
+        for (int n = 1; n <= 5; n++)
+            System.out.println("n = " + n
+                               + ", n^2 = " + square(n));
+        
+    }
+    public static int square(int n){
+        if(n<0)
+        n=-n;
+
+        int res=n;
+
+        for(int i=1;i<n;i++){
+            res+=n;
+
+           // return res;
+        }
+        return res;
+    }
+    
+}
