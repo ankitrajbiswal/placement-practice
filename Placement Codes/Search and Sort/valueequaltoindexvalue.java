f16946df8c1c19d221235dfa2a2fde166fbcb9c5
Ankit Raj Biswal <ankitrudra2001@gmail.com>
2022-04-16 06:36:56 -0700

    Create valueequaltoindexvalue.java

---
+++ Placement Codes/Search and Sort/valueequaltoindexvalue.java
@@ -0,0 +1,12 @@
+class Solution {
+    ArrayList<Integer> valueEqualToIndex(int arr[], int n) {
+        // code here
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(i+1==arr[i])
+            list.add(i+1);
+        }
+        return list;
+    }
+
+}
