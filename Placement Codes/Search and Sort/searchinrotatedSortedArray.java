58b089e41c6a4f567481597fce3394ff49103f19
Ankit Raj Biswal <ankitrudra2001@gmail.com>
2022-04-16 15:48:32 +0530

    Add files via upload

---
+++ Placement Codes/Search and Sort/searchinrotatedSortedArray.java
@@ -0,0 +1,16 @@
+public class searchinrotatedSortedArray {
+    public static void main(String[] args){
+        int nums[]={4,5,6,7,0,1,2};
+        System.out.println(search(nums,9));
+    }
+
+    public static int search(int [] nums, int target){
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]==target)
+            return i;
+        }
+        return -1;
+    }
+    
+}
